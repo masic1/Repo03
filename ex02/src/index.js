fbd55a8f4d28c31f589a125e8390ff4e74ca7ea8
masic1 <masic.mirza@gmail.com>
2021-09-15 23:34:12 +0200

    Adding exercises

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    // Create a project using steps from Exercise 01.
+    /* 
+        Create both type of comments in the file index.js
+        Have the following setup as the step 1 for the index.js file:
+    */ 
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+        var x = 7;
+        var y = 12;
+        var z = "My name is";
+    // Only change code above this line
+    x = x + 2;
+    y = y + 4;
+    z = z + " John!";
+}
+
+console.log(main());
+
+module.exports = main;
